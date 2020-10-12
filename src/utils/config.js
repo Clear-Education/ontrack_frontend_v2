0b266d354d5279a029c2cf6c2058c06526fff631
mariano <mariano.lopez@almundo.com>
2020-10-12 20:18:25 -0300

    urls

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 const config = {
-    api_url: "https://ontrack-education.herokuapp.com/api",
+    api_url: "https://ontrack-production.herokuapp.com/api",
     default_picture: "https://c8.alamy.com/comp/P9MYWR/man-avatar-profile-P9MYWR.jpg",
-    picture_path: "https://ontrack-education.herokuapp.com"
+    picture_path: "https://ontrack-production.herokuapp.com"
 }
 
 export default config
