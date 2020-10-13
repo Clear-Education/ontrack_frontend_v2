879b4232b37567427fa84dca9829fbed354e0fb3
mariano <mariano.lopez@almundo.com>
2020-10-13 14:23:06 -0300

    url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 const config = {
-    api_url: "https://ontrack-education.herokuapp.com/api",
+    api_url: "https://ontrack-production.herokuapp.com/api",
     default_picture: "https://c8.alamy.com/comp/P9MYWR/man-avatar-profile-P9MYWR.jpg"
 }
 
